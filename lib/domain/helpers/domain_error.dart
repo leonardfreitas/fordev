85ee54e0a32d0ffa8dc3e44a4a9b3fcbf539dea8
Leonardo Freitas <leonardofreitasdev@gmail.com>
2020-12-12 17:12:23 -0300

    ensure StreamLoginPresenter emits correct events on InvalidCredentialsError

--- lib/domain/helpers/domain_error.dart
+++ lib/domain/helpers/domain_error.dart
@@ -1,1 +1,14 @@
+import 'package:fordev/domain/helpers/helpers.dart';
+
 enum DomainError { unexpected, invalidCredentials }
+
+extension DomainErrorExtension on DomainError {
+  String get description {
+    switch (this) {
+      case DomainError.invalidCredentials:
+        return 'Credenciais Inválidas';
+      default:
+        return '';
+    }
+  }
+}

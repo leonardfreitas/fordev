d783d82b205aec135bd5fd42db0e9c76f2b791b4
Leonardo Freitas <leonardofreitasdev@gmail.com>
2020-12-12 17:14:48 -0300

    ensure StreamLoginPresenter emits correct events on UnexpectedError

--- lib/domain/helpers/domain_error.dart
+++ lib/domain/helpers/domain_error.dart
@@ -6,7 +6,9 @@ extension DomainErrorExtension on DomainError {
   String get description {
     switch (this) {
       case DomainError.invalidCredentials:
-        return 'Credenciais Inválidas';
+        return 'Credenciais Inválidas.';
+      case DomainError.unexpected:
+        return 'Algo errado aconteceu. Tente novamente em breve.';
       default:
         return '';
     }

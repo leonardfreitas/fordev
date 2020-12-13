1752fc70fab39ac57f5c201e12e46b1edb9d0ba9
Leonardo Freitas <leonardofreitasdev@gmail.com>
2020-12-13 18:45:30 -0300

    move ValidationComposite to own file

--- test/validation/validators/validation_composite_test.dart
+++ test/validation/validators/validation_composite_test.dart
@@ -1,26 +1,8 @@
-import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
-import 'package:fordev/validation/protocols/field_validation.dart';
-import 'package:fordev/presentations/protocols/protocols.dart';
-
-class ValidationComposite implements Validation {
-  final List<FieldValidation> validations;
-
-  ValidationComposite(this.validations);
-
-  String validate({@required String field, @required String value}) {
-    String error;
-    for (final validation in validations.where((v) => v.field == field)) {
-      error = validation.validate(value);
-      if (error?.isNotEmpty == true) {
-        return error;
-      }
-    }
-    return error;
-  }
-}
+import 'package:fordev/validation/validators/validators.dart';
+import 'package:fordev/validation/protocols/protocols.dart';
 
 class FieldValidationSpy extends Mock implements FieldValidation {}
 

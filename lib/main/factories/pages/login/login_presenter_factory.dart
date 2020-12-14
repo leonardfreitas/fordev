8b521d0e47a62cc2c54d1a874edfa173105e29d7
Leonardo Freitas <leonardofreitasdev@gmail.com>
2020-12-14 17:32:16 -0300

    add GetxLoginPresenter

--- lib/main/factories/pages/login/login_presenter_factory.dart
+++ lib/main/factories/pages/login/login_presenter_factory.dart
@@ -2,8 +2,15 @@ import '../../../../presentations/presenters/presenters.dart';
 import '../../../../ui/pages/login/login_presenter.dart';
 import '../../factories.dart';
 
-LoginPresenter makeLoginPresenter() {
+LoginPresenter makeStreamLoginPresenter() {
   return StreamLoginPresenter(
+    authentication: makeRemoteAuthentication(),
+    validation: makeLoginValidation(),
+  );
+}
+
+LoginPresenter makeGetxLoginPresenter() {
+  return GetxLoginPresenter(
     authentication: makeRemoteAuthentication(),
     validation: makeLoginValidation(),
   );
